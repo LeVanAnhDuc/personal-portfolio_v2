5f7a0b8b9f17085b932b4915bb9d363aeec9990a
LrVanAnhDuc <levananhduc1804@gmail.com>
2024-01-24 12:02:46 +0700

    feat (project)

--- src/assets/img/projects/index.ts
+++ src/assets/img/projects/index.ts
@@ -1,15 +1,19 @@
 import admin_duck from './admin_duck.png';
+import admin_leduc from './admin_leduc.png';
 import admin_stellar from './admin_stellar.png';
 import personal_portfolio from './personal_portfolio.png';
 import user_duck from './user_duck.png';
 import user_stellar from './user_stellar.png';
+import user_leduc from './user_leduc.png';
 import personal_portfolio_v2 from './personal_portfolio_v2.png';
 
 export const imgProject = {
     admin_duck,
     admin_stellar,
+    admin_leduc,
     personal_portfolio,
     user_duck,
     user_stellar,
     personal_portfolio_v2,
+    user_leduc,
 };
--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -6,11 +6,36 @@ import CardProject from '../../components/CardProject';
 const Project = () => {
     const { t } = useTranslation('project');
     return (
-        <main className="flex flex-col items-center">
+        <main className="flex flex-col items-center py-10 px-8 md:px-10 lg:px-20 xl:px-32">
             {/* section 1 */}
-            <p className="text-primary-100 font-bold text-xl uppercase">{t('projects')}</p>
-            <h1 className="mb-5">{t('desciptionProjects')}</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+            <p className="text-primary-800 font-bold text-4xl uppercase mb-5">{t('desciptionProjects')}</p>
+            {/* <h1 className="mb-5">{t('desciptionProjects')}</h1> */}
+            <div className="grid grid-cols-1  lg:grid-cols-2 gap-5">
+                <CardProject
+                    linkGithub="https://github.com/LeVanAnhDuc/personal-portfolio_v2"
+                    linkImage={imgProject.personal_portfolio_v2}
+                    linkProject="https://personal-portfolio-v2-nine.vercel.app/"
+                    role={t('websiteIntroducingMyself.role')}
+                    technical="React, TypeScript, AntD, Tailwindcss, i18next, Framer-motion, Redux Toolkit, React-hook-form,..."
+                    title={t('websiteIntroducingMyself.title') + ' (v.2)'}
+                    description={t('websiteIntroducingMyself.description')}
+                />
+                <CardProject
+                    linkGithub="https://github.com/LeVanAnhDuc/eCom-intern-2/tree/main/client/user"
+                    linkImage={imgProject.user_leduc}
+                    role={t('LeDucWebsite.role')}
+                    technical="React, i18next, TypeScript, framer-motion, MUI, swiper, Tailwindcss, React-hook-form, yub,  Redux Toolkit,..."
+                    title={t('LeDucWebsite.title')}
+                    description={t('LeDucWebsite.description')}
+                />
+                <CardProject
+                    linkGithub="https://github.com/LeVanAnhDuc/eCom-intern-2/tree/main/client/admin"
+                    linkImage={imgProject.admin_leduc}
+                    role={t('LeDucWebsite.role')}
+                    technical="React, i18next, TypeScript, framer-motion, MUI, Tailwindcss, React-hook-form, yub,..."
+                    title={t('LeDucWebsite.title')}
+                    description={t('LeDucWebsite.description')}
+                />
                 <CardProject
                     linkGithub="https://github.com/LeVanAnhDuc/fe-user-TLCN"
                     linkImage={imgProject.user_duck}
@@ -30,6 +55,15 @@ const Project = () => {
                     description={t('DuckFashionWebsiteAdmin.description')}
                 />
                 <CardProject
+                    linkGithub="https://github.com/LeVanAnhDuc/Personal_Portfolio"
+                    linkImage={imgProject.personal_portfolio}
+                    linkProject="https://personal-portfolio-ten-red.vercel.app/"
+                    role={t('websiteIntroducingMyself.role')}
+                    technical="Next, TypeScript, Antd, Tailwindcss..."
+                    title={t('websiteIntroducingMyself.title') + ' (v1)'}
+                    description={t('websiteIntroducingMyself.description')}
+                />
+                <CardProject
                     linkGithub="https://github.com/LeVanAnhDuc/fe-stellar"
                     linkImage={imgProject.user_stellar}
                     linkProject="https://fe-stellar.vercel.app/"
@@ -46,15 +80,6 @@ const Project = () => {
                     technical="React, React Bootstrap, Redux/toolkit, Scss ..."
                     title={t('StellarHotelWebsiteAdmin.title')}
                     description={t('StellarHotelWebsiteAdmin.description')}
-                />
-                <CardProject
-                    linkGithub="https://github.com/LeVanAnhDuc/Personal_Portfolio"
-                    linkImage={imgProject.personal_portfolio}
-                    linkProject="https://personal-portfolio-ten-red.vercel.app/"
-                    role={t('websiteIntroducingMyself.role')}
-                    technical="Next, TypeScript, Antd, Tailwindcss..."
-                    title={t('websiteIntroducingMyself.title')}
-                    description={t('websiteIntroducingMyself.description')}
                 />
             </div>
         </main>
